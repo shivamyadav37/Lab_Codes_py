247f225110e57ce59c280551b87ee0b0c31ecc80
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-01-21 18:52:15 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,6 @@
+'''Program to input two integer values and calculate first number raised to the power second
+number.'''
+num1 = int(input("first\n"))
+num2 = int(input("second\n"))
+pow = num1**num2
+print(pow)
