24cc4f2c1aa9207d6224ea27e1ebb83e87d9362c
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-01-22 18:21:45 +0530

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,9 @@
+''' Program to find the Compound Interest compounded annually and the total amount when the
+Principal, Rate of Interest and Time are entered by the user.'''
+p = int(input("principal\n"))
+r = float(input("rate\n"))
+n = int(input("periods\n"))
+
+c = p*((1+r)**n -1)
+#user can change formula for compound interest according to their need.
+print("compound interest",c)
