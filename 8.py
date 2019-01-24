d94ef94e08fc3d74272a9d70f7a6c774531ef9b2
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-01-24 18:37:08 +0530

    Finding area and circumference of a Circle

--- 8.py
+++ 8.py
@@ -1,5 +1,5 @@
 ''' Program to find the area and circumference of a circle, when the radius is entered by the user.
-However, the user can input radius in integer or float.'''
+However, the user can input radius in integer or float'''
 radius = float(input("Radius\n"))
 pi = 3.14
 area = pi*(radius**2)
