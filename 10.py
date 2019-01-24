6158bb3f70e156444e89a51c1269f227b3ec861a
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-01-24 18:37:40 +0530

    Swapping of Two numbers

--- 10.py
+++ 10.py
@@ -1,4 +1,4 @@
-#Program to input two numbers and print the swapped values of them.
+#Program to input two numbers and print the swapped values of them
 a = int(input("enter first number\n"))
 b = int(input("enter second number\n"))
 a,b = b,a
