8dd20e45928fb77cf9282aa6a4bd52d3a713aec5
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-01-24 18:34:39 +0530

    Exponents

--- 6.py
+++ 6.py
@@ -1,5 +1,5 @@
 '''Program to input two integer values and calculate first number raised to the power second
-number.'''
+number'''
 num1 = int(input("first\n"))
 num2 = int(input("second\n"))
 pow = num1**num2
