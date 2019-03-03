c3001c4e8e3d22295bd54cac65da96f2a849212a
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-03-03 16:07:27 +0530

    A Program to display hexadecimal,octal and squareroot values

---
+++ 20.py
@@ -0,0 +1,10 @@
+#Author--> Shivam Yadav
+#Problem-->
+#Write a Program to enter a number and display it's hexadecimal,octal and squareroot values
+
+#Program-->
+
+n=int(input("Enter a Number\n"))
+print("Hexadecimal of ",n,"is",hex(n))
+print("Ocatal of ",n,"is",oct(n))
+print("Squareroot of ",n,"is",(n)**0.5)
