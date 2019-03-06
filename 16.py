48ec11684964256cdc04a56c8e8fd629c10df04b
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-03-06 19:32:53 +0530

    Digit_at_One's_Place

---
+++ 16.py
@@ -0,0 +1,8 @@
+#Author--> Shivam Yadav
+#Problem--> Program to print digit at one's place of a Number.
+
+#Program-->
+
+n=int(input("Enter the number\n"))
+result=n%10
+print("The digit at One's place is:",result)
