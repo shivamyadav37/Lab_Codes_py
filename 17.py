24996e1d7f33acf8603d837801337ba7ebdd816b
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-03-06 19:47:23 +0530

    Average_and_Deviation

---
+++ 17.py
@@ -0,0 +1,13 @@
+#Author--> Shivam Yadav
+#Problem--> Program to calculate average of two numbers and print ther deviation.
+
+#Program-->
+
+n=int(input("Enter the first number\n"))
+m=int(input("Enter the first number\n"))
+avg=(n+m)/2
+dev1=n-avg
+dev2=m-avg
+print("Average is:",avg)
+print("Deviation of first number is:",dev1)
+print("Deviation of second number is:",dev2)
