c831f71901325f61007708d0a0557ffdeee3f779
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-03-27 11:13:57 +0530

    Delete Exponents.py

--- Exponents.py
+++
@@ -1,6 +0,0 @@
-'''Program to input two integer values and calculate first number raised to the power second
-number'''
-num1 = int(input("first\n"))
-num2 = int(input("second\n"))
-pow = num1**num2
-print(pow)
