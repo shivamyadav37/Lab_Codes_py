59891fc7e9bc567f83ebaec1355d60ff39ddce56
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-04-03 12:59:59 +0530

    Delete 8.py

--- 8.py
+++
@@ -1,8 +0,0 @@
-''' Program to find the area and circumference of a circle, when the radius is entered by the user.
-However, the user can input radius in integer or float'''
-radius = float(input("Radius\n"))
-pi = 3.14
-area = pi*(radius**2)
-circm = 2*pi*radius 
-print(area)
-print(circm)
